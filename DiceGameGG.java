7b9346203d99d0c362a8c3ac6c5b4bf6dc0dd1e3
HirotakaKosai <s1260155@u-aizu.ac.jp>
2019-07-11 13:47:02 +0900

    Won/lost+Name message  added

---
+++ DiceGameGG.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class DiceGameGG{
+	public static void main(String[] args) {
+	Random r = new Random();
+	
+	int a = r.nextInt(6);
+	int b = r.nextInt(6);
+	int total=a+b;
+	
+	System.out.println("What is your name ?");
+
+	Scanner scan = new Scanner(System.in);
+	System.out.print(">");
+	String str = scan.next();
+	System.out.println("Hello, " + str + "!");
+	
+	
+	System.out.println("Rolling the dice...");
+	System.out.println("Die 1:"+a);
+	System.out.println("Die 2:"+b);
+	System.out.println("Total Value:"+total);
+	if(total>7) {
+		System.out.println( str + " won!");
+	}else {
+		System.out.println( str + " lose");
+	}
+	
+	}
+}
