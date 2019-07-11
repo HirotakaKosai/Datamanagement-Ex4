2f5d13ad23fee7d4af94527eafa68c66381f2b64
HirotakaKosai <s1260155@u-aizu.ac.jp>
2019-07-11 13:15:39 +0900

    Greetings added

---
+++ DiceGameGreet.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class DiceGameGreet{
+	public static void main(String[] args) {
+	Random r = new Random();
+	
+	int a = r.nextInt(6);
+	int b = r.nextInt(6);
+	int total=a+b;
+	
+	System.out.println("What is your name ?");
+
+	Scanner scan = new Scanner(System.in);
+	System.out.print(">");
+	String str = scan.next();
+	System.out.println("Hello, " + str + "!");
+	
+	
+	
+	System.out.println("Rolling the dice...");
+	System.out.println("Die 1:"+a);
+	System.out.println("Die 2:"+b);
+	System.out.println("Total Value:"+total);
+	}
+}
